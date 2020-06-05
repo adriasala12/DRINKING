9ad04947e14d277433623c6a22cd1f80078174b3
Adrià Sala <adriasala124@gmail.com>
2020-06-05 11:42:01 +0200

    Initial Commit

---
+++ DRINKING/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DRINKING
+//
+//  Created by Adrià Sala Roget on 05/06/2020.
+//  Copyright © 2020 Adrià Sala Roget. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
