76373c15e7ee35c1b8fb6b3a24ecff8bc88a1be7
Adrià Sala <adriasala124@gmail.com>
2020-06-06 12:38:59 +0200

    Added Game View Controller

---
+++ DRINKING/Controllers/GameViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GameViewController.swift
+//  DRINKING
+//
+//  Created by Adrià Sala Roget on 06/06/2020.
+//  Copyright © 2020 Adrià Sala Roget. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+    
+    
+
+}
