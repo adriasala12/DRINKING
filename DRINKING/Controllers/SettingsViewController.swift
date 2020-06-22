db0a8b701eb5368015170412cef3bdf5eae1054e
Adrià Sala <adriasala124@gmail.com>
2020-06-22 23:24:24 +0200

    Added color selector images to assets. Created SettingsViewController (and finished editing)

---
+++ DRINKING/Controllers/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  DRINKING
+//
+//  Created by Adrià Sala Roget on 22/06/2020.
+//  Copyright © 2020 Adrià Sala Roget. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
